630a50070fd0c4aa866253e89887893278417ec7
nguyenngochoan <nguyenngochoan@cityascom.vn>
2026-01-06 15:36:14 +0700

    Commit react native source

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+              Xin chào! Build ios bằng gihub action{"\n"}
+              Phát triển bởi Hoàn CAV ❤️
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
